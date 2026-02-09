dd58ff0167d9e11b207afa2a45da7a78c2907339
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2026-02-09 11:46:41 +0100

    feat: improve syntax highliting

--- web/src/utils/prism.ts
+++ web/src/utils/prism.ts
@@ -7,6 +7,8 @@ import 'prismjs/components/prism-typescript'
 import 'prismjs/components/prism-jsx'
 import 'prismjs/components/prism-tsx'
 import 'prismjs/components/prism-css'
+import 'prismjs/components/prism-scss'
+import 'prismjs/components/prism-sass'
 import 'prismjs/components/prism-json'
 import 'prismjs/components/prism-yaml'
 import 'prismjs/components/prism-bash'
@@ -15,6 +17,32 @@ import 'prismjs/components/prism-java'
 import 'prismjs/components/prism-rust'
 import 'prismjs/components/prism-markdown'
 import 'prismjs/components/prism-diff'
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-ruby'
+import 'prismjs/components/prism-c'
+import 'prismjs/components/prism-cpp'
+import 'prismjs/components/prism-csharp'
+import 'prismjs/components/prism-sql'
+import 'prismjs/components/prism-swift'
+import 'prismjs/components/prism-kotlin'
+import 'prismjs/components/prism-scala'
+import 'prismjs/components/prism-r'
+import 'prismjs/components/prism-perl'
+import 'prismjs/components/prism-docker'
+import 'prismjs/components/prism-nginx'
+import 'prismjs/components/prism-toml'
+import 'prismjs/components/prism-ini'
+import 'prismjs/components/prism-graphql'
+import 'prismjs/components/prism-elixir'
+import 'prismjs/components/prism-erlang'
+import 'prismjs/components/prism-haskell'
+import 'prismjs/components/prism-lua'
+import 'prismjs/components/prism-dart'
+import 'prismjs/components/prism-powershell'
+import 'prismjs/components/prism-vim'
+import 'prismjs/components/prism-makefile'
+import 'prismjs/components/prism-protobuf'
 
 export function getLanguageFromFilename(filename: string): string {
   const ext = filename.split('.').pop()?.toLowerCase() ?? ''
@@ -29,7 +57,8 @@ export function getLanguageFromFilename(filename: string): string {
     'java': 'java',
     'rs': 'rust',
     'css': 'css',
-    'scss': 'css',
+    'scss': 'scss',
+    'sass': 'sass',
     'json': 'json',
     'yaml': 'yaml',
     'yml': 'yaml',
@@ -43,8 +72,34 @@ export function getLanguageFromFilename(filename: string): string {
     'php': 'php',
     'c': 'c',
     'cpp': 'cpp',
+    'cc': 'cpp',
+    'cxx': 'cpp',
     'h': 'c',
     'hpp': 'cpp',
+    'cs': 'csharp',
+    'swift': 'swift',
+    'kt': 'kotlin',
+    'kts': 'kotlin',
+    'scala': 'scala',
+    'r': 'r',
+    'pl': 'perl',
+    'dockerfile': 'docker',
+    'toml': 'toml',
+    'ini': 'ini',
+    'graphql': 'graphql',
+    'gql': 'graphql',
+    'ex': 'elixir',
+    'exs': 'elixir',
+    'erl': 'erlang',
+    'hrl': 'erlang',
+    'hs': 'haskell',
+    'lua': 'lua',
+    'dart': 'dart',
+    'ps1': 'powershell',
+    'vim': 'vim',
+    'makefile': 'makefile',
+    'mk': 'makefile',
+    'proto': 'protobuf',
   }
 
   return languageMap[ext] || 'plaintext'
